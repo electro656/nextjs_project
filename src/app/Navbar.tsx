1e651811041f8411e0d7fd1427793c9c9657cda2
electro656 <nguyenhaipham1982@gmail.com>
2024-07-12 13:20:36 +0700

    Big Update: - Navbar finished - Active link styled - MySQL and Prisma added - Issue model created - Postman installed and API made

---
+++ src/app/Navbar.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+import { AiFillBug } from "react-icons/ai";
+import classnames from "classnames";
+
+function Navbar() {
+  const currentPath = usePathname();
+  // console.log(currentPath);
+
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues" },
+  ];
+
+  return (
+    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
+      <Link href="/">
+        <AiFillBug />
+      </Link>
+      <ul className="flex space-x-6">
+        {links.map((link) => (
+          <li key={link.href}>
+            <Link
+              key={link.href}
+              className={classnames({
+                "text-zinc-900 border-2 border-indigo-500 rounded-lg p-1":
+                  link.href === currentPath,
+                "text-zinc-500": link.href !== currentPath,
+                "hover:text-zinc-800 transition-colors": true,
+              })}
+              href={link.href}
+            >
+              {link.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
